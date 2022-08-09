514f13c1f88e661a896be0b33984b53194a33d3e
anagdinizp <anagabriela0399@hotmail.com>
2022-08-09 18:39:55 -0300

    Colocando a função de retirar o dropdown quando o usuário clicar em outro lugar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,11 @@
 import { Avatar } from "@material-tailwind/react";
 
-interface size{
+interface types{
   width: string;
   height: string;
 }
 
-export function UserAvatar({width, height}: size) {
+export function UserAvatar({width, height}: types) {
   return (
     <div className="flex">
       <Avatar className="rounded-full cursor-pointer" src="https://avatars.githubusercontent.com/u/79330582?v=4" alt="Avatar do usuário" variant="circular" width={width} height={height}/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import { Ancor } from "./Anchor";
 import { UserAvatar } from "./Avatar";
 import { DropdownUser } from "./Dropdow";
@@ -6,9 +7,25 @@ import { Logo } from "./Logo";
 
 export function Navbar({ children }: any) {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    
+    return () => document.removeEventListener("click", handleClick);
+    
+    function handleClick(event: any) {
+      if (ref && ref.current) {
+        const myRef: any = ref.current;
+        if (!myRef.contains(event.target)) {
+          setOpen(false);
+        }
+      }
+    }
+  });
 
   return (
-    <div className="flex items-center h-16 justify-around bg-yellow-300 border-b border-b-gray-800">
+    <div className="flex items-center h-16 justify-around bg-yellow-100 border-b border-b-gray-800">
       <Logo width={"150"} height={"150"} />
       <ul className="flex items-center gap-8 text-black">
         <li className="flex items-center justify-center">
@@ -18,10 +35,10 @@ export function Navbar({ children }: any) {
           <Ancor href={"#"} text={"Meus crushs"} icon={"crushs"} />
         </li>
         <li>
-            <Ancor href={"#"} text={"Pretendentes"} icon={"suitors"} />
+          <Ancor href={"#"} text={"Pretendentes"} icon={"suitors"} />
         </li>
       </ul>
-      <div onClick={() => setOpen(!open)}>
+      <div  ref={ref as any} onClick={() => setOpen(!open)}>
         <UserAvatar width={"44rem"} height={"14rem"} />
         {open && <DropdownUser />}
       </div>
