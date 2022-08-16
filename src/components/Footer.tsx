016389506e2549d9c1e1237688270cb0734bc338
anagdinizp <anagabriela0399@hotmail.com>
2022-08-16 11:54:01 -0300

    Corrigindo a posição do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { LogoFooter } from "./Logo";
 
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 p-1 bg-yellow-100 flex w-full justify-center text-center border border-t-gray-800">
+    <footer className="flex shrink-0 p-1 w-full justify-center text-center xl:absolute bottom-0 bg-yellow-100  border border-t-gray-800">
       <LogoFooter />
     </footer>
   )
